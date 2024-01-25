33dcb263fee66322ff980d472b7aff97a789e289
Lorenzo G <info@enzogonzalez.es>
2024-01-25 15:58:44 +0100

    removing canvas dnd-kit content

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,6 +1,5 @@
 import { usePinsStore } from "@/store/PinStore";
 import { Card } from "../ui/card";
-import { DndContext, useDndMonitor, useDroppable } from "@dnd-kit/core";
 
 import Pin from "./Pin";
 import { pinsToCss } from "@/utils/colorUtils";
@@ -13,24 +12,14 @@ export function Canvas() {
 
   const gradientCanvas = pinsToCss(pinList);
 
-  const { setNodeRef } = useDroppable({
-    id: "droppable",
-    data: {
-      accepts: ["type1", "type2"],
-    },
-  });
-
   return (
-    <DndContext>
-      <Card
-        id="canvas"
-        className="w-full h-full transition-all relative"
-        style={{ background: gradientCanvas }}
-        onClick={(e) => handleCreatePin(e)}
-        ref={setNodeRef}
-      >
-        {isPinsVisible && pinList.map((pin) => <Pin key={pin.id} {...pin} />)}
-      </Card>
-    </DndContext>
+    <Card
+      id="canvas"
+      className="w-full h-full transition-all relative"
+      style={{ background: gradientCanvas }}
+      onClick={(e) => handleCreatePin(e)}
+    >
+      {isPinsVisible && pinList.map((pin) => <Pin key={pin.id} {...pin} />)}
+    </Card>
   );
 }
